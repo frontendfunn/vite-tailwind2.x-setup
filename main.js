dd2ac1961ea7e41ed89c62c2097f3c4a8a4a9db3
frontendfunn <frontendfunn@gmail.com>
2021-06-07 14:43:27 +0530

    top navbar code added

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
+import "highlight.js/styles/ir-black.css";
 import "./style.css";
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import xml from "highlight.js/lib/languages/xml";
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("xml", xml);
 import "alpinejs";
+hljs.highlightAll();
