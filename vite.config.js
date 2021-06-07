42de0eb0bbc887f2e35c5a647d3ccefcd5378894
frontendfunn <frontendfunn@gmail.com>
2021-06-07 13:37:12 +0530

    top navbar added

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+const { resolve } = require("path");
 
 export default defineConfig({
   base: "/vite-tailwind2.x-setup/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        topNavbar: resolve(__dirname, "pages/topNavbar/index.html"),
+      },
+    },
+  },
 });
